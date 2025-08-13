53cb65aa9f0380d97a4c1f6f48da5512ec7193df
william <mbonguefeukou@gmail.com>
2025-08-13 02:54:40 +0200

    finish

--- nextjs-dashboard/app/dashboard/customers/page.tsx
+++ nextjs-dashboard/app/dashboard/customers/page.tsx
@@ -1,3 +1,15 @@
-export default function Customers() {
-  return <p>Customers Page</p>
+import { fetchFilteredCustomers } from "@/app/lib/data";
+import Table from "@/app/ui/customers/table";
+
+export default async function Page() {
+  const customers = await fetchFilteredCustomers('');
+
+  return (
+    <main>
+      <h1 className="text-2xl font-bold">Customers</h1>
+      <div className="mt-4">
+        <Table customers={customers} />
+      </div>
+    </main>
+  );
 }
--- nextjs-dashboard/app/lib/action.ts
+++ nextjs-dashboard/app/lib/action.ts
@@ -3,6 +3,8 @@ import { z } from 'zod';
 import postgres from 'postgres';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
+import { signIn } from '@/auth';
+import { AuthError } from 'next-auth';
 
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 
@@ -104,4 +106,24 @@ export async function deleteInvoice(id: string) {
     // throw new Error('Delete action is not implemented yet.');
     await sql`DELETE FROM invoices WHERE id = ${id}`;
     revalidatePath('/dashboard/invoices');
+}
+
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData,
+) {
+  try {
+    await signIn('credentials', formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case 'CredentialsSignin':
+          return 'Invalid credentials.';
+        default:
+          return 'Something went wrong.';
+      }
+    }
+    throw error;
+  }
 }
